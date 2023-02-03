a85186e34a4280f9c917d87f6fcb9d23f5e7e040
Martin Pitt <martin@piware.de>
2023-02-03 10:48:07 +0100

    test: Keep container alive with $DEBUG

--- test/run-container.sh
+++ test/run-container.sh
@@ -4,4 +4,5 @@ set -eu
 podman run -i --rm -v .:/source -w /source ${1:-registry.fedoraproject.org/fedora:latest} <<EOF
 dnf install -y cargo clippy rustfmt
 test/run.sh
+[ -z "${DEBUG:-}" ] || sleep infinity
 EOF
